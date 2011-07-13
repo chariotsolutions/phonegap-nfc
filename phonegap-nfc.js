683a846e54bbda9af4128895c64137c97a7021a6
Don Coleman <dcoleman@chariotsolutions.com>
2011-07-13 12:05:54 -0400

    addService is replaced by plugins.xml

--- phonegap-nfc.js
+++ phonegap-nfc.js
@@ -153,17 +153,3 @@ navigator.nfc = {
         document.dispatchEvent(e);        
     }     
 };
-
-/**
- * <ul>
- * <li>Register the NFC Javascript plugin.</li>
- * <li>Also register native call which will be called when this plugin runs</li>
- * </ul>
- */
-PhoneGap.addConstructor(function() { 
-  // Register the javascript plugin with PhoneGap
-  PhoneGap.addPlugin('NdefPlugin', navigator.nfc);
-
-  // Register the native class of plugin with PhoneGap
-  navigator.app.addService("NdefPlugin", "com.chariotsolutions.nfc.plugin.NdefPlugin"); 
-});
