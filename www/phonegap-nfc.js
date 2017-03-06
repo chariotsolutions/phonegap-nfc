3baa490918ef4cb93a6e6401fd2b1e4e1d950ee5
Wesley Van De Voorde <vandevoordewesley@gmail.com>
2017-03-06 09:53:44 +0100

    bug fix

--- www/phonegap-nfc.js
+++ www/phonegap-nfc.js
@@ -1,4 +1,3 @@
-cordova.define('phonegap-nfc.NFC', function (require, exports, module) {
 /* jshint  bitwise: false, camelcase: false, quotmark: false, unused: vars */
 /* global cordova, console */
     'use strict';
