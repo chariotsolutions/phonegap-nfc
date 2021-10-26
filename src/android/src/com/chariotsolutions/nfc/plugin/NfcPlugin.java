c22e03c2d2d859e458708875ace29880445a7e9c
Mesbah <mesbah@mowlavi.ca>
2021-10-26 15:44:54 -0700

    Prevent NPE on sendEvent

--- src/android/src/com/chariotsolutions/nfc/plugin/NfcPlugin.java
+++ src/android/src/com/chariotsolutions/nfc/plugin/NfcPlugin.java
@@ -737,7 +737,7 @@ public class NfcPlugin extends CordovaPlugin implements NfcAdapter.OnNdefPushCom
 
             PluginResult result = new PluginResult(PluginResult.Status.OK, event);
             result.setKeepCallback(true);
-            channelCallback.sendPluginResult(result);
+            if (channelCallback != null) channelCallback.sendPluginResult(result);
         } catch (JSONException e) {
             Log.e(TAG, "Error sending NFC event through the channel", e);
         }
